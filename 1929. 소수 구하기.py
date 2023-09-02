af6ea3e9af0c54a6602421fa2d8503b04dd3ae94
Minkyung Ko <komg001234@gmail.com>
2023-09-02 16:06:44 +0900

    Second try

--- 1929. 소수 구하기.py
+++ 1929. 소수 구하기.py
@@ -1,12 +1,10 @@
 n, m = map(int, input().split())
 
 for i in range(n, m+1):
-    j = 1
-    cnt = 0
-    while j < i-1:
-        j += 1
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5)+1):
         if(i % j == 0):
-            cnt += 1
-    if cnt == 0:
+            break
+    else:
         print(i)
-            
